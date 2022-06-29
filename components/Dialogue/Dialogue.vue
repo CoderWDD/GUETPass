be38043709ab9d70a02bd9a850ed8fdf4119e4a5
yunduo23 <2992588618@qq.com>
2022-06-29 17:53:18 +0800

    homepage container

---
+++ components/Dialogue/Dialogue.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="dialogue">
+
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "Dialogue",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style>
+	.dialogue {
+		position: relative;
+		height: 70px;
+		width: auto;
+		background-color: white;
+		text-align: center;
+	}
+
+	.dialogue::after {
+		content: '';
+		position: absolute;
+		left: 20%;
+		top: 69px;
+		right: auto;
+		height: 2px;
+		width: 80%;
+		background-color: gainsboro;
+	}
+</style>
---
+++ pages/HomePage/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<view class="container">
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+			<Dialogue></Dialogue>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				item: []
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.container{
+	display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+}
+</style>
