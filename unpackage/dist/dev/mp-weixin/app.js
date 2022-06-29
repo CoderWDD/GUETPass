d0747ceaf358efb456fc5bedfecd463b13689e38
CoderWdd <CoderWdd@gmail.com>
2022-06-29 21:21:32 +0800

    todo

--- unpackage/dist/dev/mp-weixin/app.js
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -4,19 +4,10 @@ exports[Symbol.toStringTag] = "Module";
 var common_vendor = require("./common/vendor.js");
 if (!Math) {
   "./pages/index/index.js";
+  "./pages/HomePage/HomePage.js";
+  "./pages/HuaJiangPage/HuaJiangPage.js";
 }
-const _sfc_main = {
-  onLaunch: function() {
-    console.warn("\u5F53\u524D\u7EC4\u4EF6\u4EC5\u652F\u6301 uni_modules \u76EE\u5F55\u7ED3\u6784 \uFF0C\u8BF7\u5347\u7EA7 HBuilderX \u5230 3.1.0 \u7248\u672C\u4EE5\u4E0A\uFF01");
-    console.log("App Launch");
-  },
-  onShow: function() {
-    console.log("App Show");
-  },
-  onHide: function() {
-    console.log("App Hide");
-  }
-};
+const _sfc_main = {};
 var App = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "F:/Projects/UniappProjects/GUETPass/App.vue"]]);
 function createApp() {
   const app = common_vendor.createSSRApp(App);
---
+++ unpackage/dist/dev/mp-weixin/pages/HomePage/HomePage.js
@@ -0,0 +1,23 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  data() {
+    return {
+      item: []
+    };
+  },
+  methods: {}
+};
+if (!Array) {
+  const _easycom_Dialogue2 = common_vendor.resolveComponent("Dialogue");
+  _easycom_Dialogue2();
+}
+const _easycom_Dialogue = () => "../../components/Dialogue/Dialogue.js";
+if (!Math) {
+  _easycom_Dialogue();
+}
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {};
+}
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "F:/Projects/UniappProjects/GUETPass/pages/HomePage/HomePage.vue"]]);
+wx.createPage(MiniProgramPage);
